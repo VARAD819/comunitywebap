13775c9fbc6cff086d234c6bd5ca686d0de5735e
VARAD819 <bhandarivarad09@gmail.com>
2022-09-24 01:36:52 +0530

    host

--- comunitywebap/settings.py
+++ comunitywebap/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m^w+#tw*!xf7&crx!*$ui*p1kntt@_wp%1)6zyi+z@#)=jz7ct'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
