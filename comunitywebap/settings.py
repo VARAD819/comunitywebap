fce4932c59a08fd57ccb0711e6db2d01a91d6529
VARAD819 <bhandarivarad09@gmail.com>
2022-10-12 23:44:45 +0530

    CORS

--- comunitywebap/settings.py
+++ comunitywebap/settings.py
@@ -95,7 +95,7 @@ CSRF_TRUSTED_ORIGINS = ['*',]
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ORIGIN_WHITELIST = [ '*' ]
+CORS_ORIGIN_WHITELIST = [ '*', ]
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
